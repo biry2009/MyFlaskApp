c1ca3d06c1b67390b53f0ac9622a3a003866cce7
biry2009@gmail.com <biry2009@gmail.com>
2018-02-11 14:11:51 +0800

    add files

--- app.py
+++ app.py
@@ -1,11 +1,56 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template
+import os
+import pymysql
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost:3306/test?charset=utf8mb4'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(320), unique=True)
+    password = db.Column(db.String(32), nullable=False)
+
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    @staticmethod
+    def generate_fake(count=100):
+        from sqlalchemy.exc import IntegrityError
+        from random import seed
+        import forgery_py
+        seed()
+
+        for i in range(count):
+            u = User(username=forgery_py.name.full_name(),
+                     email=forgery_py.email.address(),
+                     password=forgery_py.basic.password())
+
+            db.session.add(u)
+            try:
+                db.session.commit()
+            except IntegrityError:
+                db.session.rollback()
+
+        return
+
+users = User.query.all()
 
 @app.route('/')
 def home_page():
-    return render_template("index.html")
+    return render_template("index.html", users=users)
 
 @app.route('/about')
 def about_page():
